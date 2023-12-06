024de762e06da9b9334e40f27cb966ef8ae8815a
KhaninaDaria22 <khanina_dasha@ukr.net>
2023-12-07 01:32:42 +0300

    commit 2

--- src/js/subscribeEmail.js
+++ src/js/subscribeEmail.js
@@ -4,13 +4,34 @@ const emailInput = document.getElementById('email');
 checkoutForm.addEventListener('submit', function(event) {
     event.preventDefault();
     if (validateEmail(emailInput.value)) {
-        alert('The order has been accepted!'); 
+        sendFormData(emailInput.value); 
       } else {
         alert('Please, please enter the correct email!');
       }
     });
 
 function validateEmail(email) {
-        var pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return pattern.test(email);
-      }
+      };
+
+function sendFormData(email) {
+    const serverUrl = 'https://food-boutique.b.goit.study/api';
+
+}
+
+
+
+async function addBook(book) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    };
+  
+    const response = await fetch(`${BASE_URL}/books`, options);
+    const newBook = await response.json();
+  
+    return newBook;
