9747da0dce33a0b7c88b5aa567b4121d23797ac4
Dima <dimasalabay0711@gmail.com>
2023-12-09 14:38:25 +0200

    import function

--- src/js/main.js
+++ src/js/main.js
@@ -13,15 +13,16 @@ import {
   appendDiscountProductsMarkup,
 } from './createMarkupMoreProducts.js';
 
+// Count of carts
+import { getCountCarts } from './header.js';
+getCountCarts();
+
 appendPopularProductsMarkup();
 appendDiscountProductsMarkup();
 
-import {createProductsMarkup} from "./createMarkup.js";
-createProductsMarkup(1, null, null)
+import { createProductsMarkup } from './createMarkup.js';
+createProductsMarkup(1, null, null);
 
-import {
-    filterCategories,
-    onSubmit
-} from "./filters.js"
+import { filterCategories, onSubmit } from './filters.js';
 filterCategories();
 onSubmit();
