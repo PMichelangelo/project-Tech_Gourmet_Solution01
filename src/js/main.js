aca3c8fcd26e35ccc6c924e05f8edb5b61ed59a3
SnehaShisNasty <katirina18@gmail.com>
2023-12-10 06:12:50 +0000

    done

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,6 @@ createProductsMarkup(1, null, null);
 import { filterCategories, filterProducts } from './filters.js';
 filterCategories();
 filterProducts();
+import { onSubmit } from './subscribeEmail';
+const emailFInput = document.querySelector('.footer-submit-btn');
+emailFInput.addEventListener('click', onSubmit);
---
+++ src/js/subscribeEmail.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { openSubcribeModal, openErrorModal } from './modal.js';
+export { onSubmit };
+let emailInput;
+function onSubmit(event) {
+  event.preventDefault();
+
+  emailInput = document.querySelector('.footer-input');
+
+  if (validateEmail(emailInput.value)) {
+    sendFormData(emailInput.value);
+  } else {
+    alert('Please enter the correct email!');
+  }
+}
+
+function validateEmail(email) {
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return pattern.test(email);
+}
+
+function sendFormData(email) {
+  const serverUrl = 'https://food-boutique.b.goit.study/api/subscription';
+
+  const formData = {
+    email: email,
+  };
+
+  axios
+    .post(serverUrl, formData)
+    .then(response => {
+      openSubcribeModal();
+    })
+    .catch(error => {
+      if (error.message.includes('409')) {
+        openErrorModal();
+      }
+    })
+    .finally(el => {
+      emailInput.value = '';
+    });
+}
