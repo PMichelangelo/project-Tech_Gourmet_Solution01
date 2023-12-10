49213648a9641465d15c25ffccd48f9b8d42b1d4
KhaninaDaria22 <khanina_dasha@ukr.net>
2023-12-10 19:41:10 +0300

    commi3

--- src/js/cartProducts.js
+++ src/js/cartProducts.js
@@ -4,42 +4,42 @@ import { createMarkup } from './createMarkup'
 const galleryEl = document.querySelector('.js-carTgallery')
 
 
-const emptyCart = document.querySelector('.cart-empty'),
-  cartList = document.querySelector('.cart-list-wrapper'),
-  cartOrderList = document.querySelector('.cart-order-list');
+// const emptyCart = document.querySelector('.cart-empty'),
+//   cartList = document.querySelector('.cart-list-wrapper'),
+//   cartOrderList = document.querySelector('.cart-order-list');
 
-async function getCardProducts(productsList) {
-  // відмальовуємо якщо пустий масив в локал сторедж
-  if (!productsList.length) {
-    emptyCart.insertAdjacentHTML(
-      'beforeend',
-      `<div class="cart-empty-desc">
-            <img
-              src="../img/shopping-basket.png"
-              srcset=""
-              alt="Shopping Basket"
-            />
-            <h2>Your basket is <span class="cart-empty-cgreen">empty...</span></h2>
-            <p class="cart-empty-text">
-              Go to the main page to select your favorite products and add them to the
-              cart.
-            </p>
-          </div>
-        `
-    );
-    // приховуємо основну розмітку
-    cartList.classList.add('visually-hidden');
-  }
+// async function getCardProducts(productsList) {
+//   // відмальовуємо якщо пустий масив в локал сторедж
+//   if (!productsList.length) {
+//     emptyCart.insertAdjacentHTML(
+//       'beforeend',
+//       `<div class="cart-empty-desc">
+//             <img
+//               src="../img/shopping-basket.png"
+//               srcset=""
+//               alt="Shopping Basket"
+//             />
+//             <h2>Your basket is <span class="cart-empty-cgreen">empty...</span></h2>
+//             <p class="cart-empty-text">
+//               Go to the main page to select your favorite products and add them to the
+//               cart.
+//             </p>
+//           </div>
+//         `
+//     );
+//     // приховуємо основну розмітку
+//     cartList.classList.add('visually-hidden');
+//   }
 
-  try {
-    const products = await Promise.all(
-      productsList.map(productId => getServerProductsById(productId))
-    );
-    const cartMarkup = createMarkup(products);
-    cartOrderList.insertAdjacentHTML('beforeend', cartMarkup);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const products = await Promise.all(
+//       productsList.map(productId => getServerProductsById(productId))
+//     );
+//     const cartMarkup = createMarkup(products);
+//     cartOrderList.insertAdjacentHTML('beforeend', cartMarkup);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export { getCardProducts };
+// export { getCardProducts };
