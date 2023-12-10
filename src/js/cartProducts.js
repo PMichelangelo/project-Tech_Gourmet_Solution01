c8b3637d93955f21a36b709979d1262f61ea7dcc
Dima <dimasalabay0711@gmail.com>
2023-12-10 12:48:10 +0200

    work with markups

--- src/js/cartProducts.js
+++ src/js/cartProducts.js
@@ -1,23 +1,42 @@
-import { getServerProductsById } from './fetchProducts'
-import { createMarkup } from './createMarkup'
+import { getServerProductsById } from './fetchProducts';
+import { createMarkup } from './createMarkup';
 
-const galleryEl = document.querySelector('.js-gallery')
-
-
+const emptyCart = document.querySelector('.cart-empty'),
+  cartList = document.querySelector('.cart-list-wrapper'),
+  cartOrderList = document.querySelector('.cart-order-list');
 
 async function getCardProducts(productsList) {
-  if (!productsList.length) galleryEl.insertAdjacentHTML('beforeend', "<span>Your cart is empty</span>")
+  // відмальовуємо якщо пустий масив в локал сторедж
+  if (!productsList.length) {
+    emptyCart.insertAdjacentHTML(
+      'beforeend',
+      `<div class="cart-empty-desc">
+            <img
+              src="../img/shopping-basket.png"
+              srcset=""
+              alt="Shopping Basket"
+            />
+            <h2>Your basket is <span class="cart-empty-cgreen">empty...</span></h2>
+            <p class="cart-empty-text">
+              Go to the main page to select your favorite products and add them to the
+              cart.
+            </p>
+          </div>
+        `
+    );
+    // приховуємо основну розмітку
+    cartList.classList.add('visually-hidden');
+  }
 
   try {
-    const products = await Promise.all(productsList.map(productId => getServerProductsById(productId)))
-    const cartMarkup = createMarkup(products)
-    galleryEl.insertAdjacentHTML('beforeend', cartMarkup)
-
+    const products = await Promise.all(
+      productsList.map(productId => getServerProductsById(productId))
+    );
+    const cartMarkup = createMarkup(products);
+    cartOrderList.insertAdjacentHTML('beforeend', cartMarkup);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-export {
-   getCardProducts
- }
+export { getCardProducts };
