147f567049463becd0f3c6c9e4c23a4739015fe1
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-12-11 12:40:08 +0200

    update pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -18,13 +18,27 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
     const paginationPages = Math.ceil(totalPages / perPage);
     console.log("paginationPages:", paginationPages);
 
+
+
     if (paginationPages === 1) {
         refs.pagination.classList.add("filters-visually-hidden");
         return
     }
 
+
+
     if (paginationPages <= 4) {
         let paginationMarkup = ``;
+        if (page === 1) {
+            refs.paginationBtnIncr.disabled = false;
+            refs.paginationBtnDecr.disabled = true;
+        } else if (page === paginationPages) {
+            refs.paginationBtnIncr.disabled = true;
+            refs.paginationBtnDecr.disabled = false;
+        } else {
+            refs.paginationBtnIncr.disabled = false;
+            refs.paginationBtnDecr.disabled = false;
+        }
         for (let i = 1; i <= paginationPages; i += 1) {
             paginationMarkup += `<li class="pagination-btn-item"><button type="button" class="pag-btn-common pagination-btn">${i}</button></li>`
         }
@@ -36,22 +50,21 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
         })
         const btns = document.querySelectorAll(".pagination-btn");
         refs.pagination.classList.remove("filters-visually-hidden");
+
+
         refs.paginationBtnIncr.addEventListener("click", e => {
-            refs.paginationBtnIncr.disabled = false;
-            refs.paginationBtnDecr.disabled = false;
             let currentPage;
             btns.forEach((btn) => {
                 if (btn.classList.contains("pagination-btn-active")) { 
                     currentPage = Number(btn.textContent);
                 }
             })
-            if (currentPage === paginationPages) {
+            if ((currentPage + 1) === paginationPages) {
                 refs.paginationBtnIncr.disabled = true;
-                console.log("no")
-                return
+                refs.paginationBtnDecr.disabled = false;
             } else {
                 refs.paginationBtnIncr.disabled = false;
-                console.log("yes")
+                refs.paginationBtnDecr.disabled = false;
             }
             getServerProducts((currentPage + 1), keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
@@ -70,6 +83,8 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                 })
             })
         })
+
+
         refs.paginationBtnDecr.addEventListener("click", e => {
             let currentPage;
             btns.forEach((btn) => {
@@ -77,14 +92,12 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                     currentPage = Number(btn.textContent);
                 }
             })
-            if (currentPage === 1) {
-                refs.paginationBtnDecr.disabled = true;
+            if ((currentPage - 1) === 1) {
                 refs.paginationBtnIncr.disabled = false;
-                console.log("no")
-                return
+                refs.paginationBtnDecr.disabled = true;
             } else {
+                refs.paginationBtnIncr.disabled = false;
                 refs.paginationBtnDecr.disabled = false;
-                console.log("yes")
             }
             getServerProducts((currentPage - 1), keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
@@ -103,11 +116,23 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                 })
             })
         })
+
+
         refs.paginationBtnList.addEventListener("click", e => {
             if (!e.target.classList.contains("pagination-btn")) {
                 return
             }
             const btnNumber = Number(e.target.textContent);
+            if (btnNumber === 1) {
+                refs.paginationBtnIncr.disabled = false;
+                refs.paginationBtnDecr.disabled = true;
+            } else if (btnNumber === paginationPages) {
+                refs.paginationBtnIncr.disabled = true;
+                refs.paginationBtnDecr.disabled = false;
+            } else {
+                refs.paginationBtnIncr.disabled = false;
+                refs.paginationBtnDecr.disabled = false;
+            }
             getServerProducts(btnNumber, keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
                 refs.productCard.innerHTML = markup;
@@ -127,7 +152,20 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
         return
     }
 
+
+
+
     if (paginationPages > 4) {
+        if (page === 1) {
+            refs.paginationBtnIncr.disabled = false;
+            refs.paginationBtnDecr.disabled = true;
+        } else if (page === paginationPages) {
+            refs.paginationBtnIncr.disabled = true;
+            refs.paginationBtnDecr.disabled = false;
+        } else {
+            refs.paginationBtnIncr.disabled = false;
+            refs.paginationBtnDecr.disabled = false;
+        }
         const paginationMarkup =
             `<li class="pagination-btn-item"><button type="button" class="pag-btn-common pagination-btn">1</button></li>
             <li class="pagination-btn-item"><button type="button" class="pag-btn-common pagination-btn">2</button></li>
@@ -143,6 +181,7 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
             }
         })
 
+
         refs.paginationBtnIncr.addEventListener("click", e => {
             let currentPage;
             btns.forEach((btn) => {
@@ -150,14 +189,12 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                     currentPage = Number(btn.textContent);
                 }
             })
-            if (currentPage === paginationPages) {
+            if ((currentPage + 1) === paginationPages) {
                 refs.paginationBtnIncr.disabled = true;
                 refs.paginationBtnDecr.disabled = false;
-                console.log("no")
-                return
             } else {
                 refs.paginationBtnIncr.disabled = false;
-                console.log("yes")
+                refs.paginationBtnDecr.disabled = false;
             }
             getServerProducts((currentPage + 1), keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
@@ -176,6 +213,8 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                 })
             })
         })
+
+
         refs.paginationBtnDecr.addEventListener("click", e => {
             let currentPage;
             btns.forEach((btn) => {
@@ -183,14 +222,12 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                     currentPage = Number(btn.textContent);
                 }
             })
-            if (currentPage === 1) {
-                refs.paginationBtnDecr.disabled = true;
+            if ((currentPage - 1) === 1) {
                 refs.paginationBtnIncr.disabled = false;
-                console.log("no")
-                return
+                refs.paginationBtnDecr.disabled = true;
             } else {
+                refs.paginationBtnIncr.disabled = false;
                 refs.paginationBtnDecr.disabled = false;
-                console.log("yes")
             }
             getServerProducts((currentPage - 1), keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
@@ -209,11 +246,23 @@ function createPaginationMarkup(totalPages, page, perPage, limit, keyword, categ
                 })
             })
         })
+
+
         refs.paginationBtnList.addEventListener("click", e => {
             if (!e.target.classList.contains("pagination-btn")) {
                 return
             }
             const btnNumber = Number(e.target.textContent);
+            if (btnNumber === 1) {
+                refs.paginationBtnIncr.disabled = false;
+                refs.paginationBtnDecr.disabled = true;
+            } else if (btnNumber === paginationPages) {
+                refs.paginationBtnIncr.disabled = true;
+                refs.paginationBtnDecr.disabled = false;
+            } else {
+                refs.paginationBtnIncr.disabled = false;
+                refs.paginationBtnDecr.disabled = false;
+            }
             getServerProducts(btnNumber, keyword, category, limit).then(({ results, totalPages, page, perPage }) => {
                 const markup = createMarkup(results);
                 refs.productCard.innerHTML = markup;
