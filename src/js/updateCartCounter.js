88183ef70d4594d5bb23c86b19a5ab7cad53b1cb
PMichelangelo <Pmichelangelo1377@gmail.com>
2023-12-11 11:46:53 +0200

    commit

--- src/js/updateCartCounter.js
+++ src/js/updateCartCounter.js
@@ -1,12 +1,15 @@
 function updateCartCounterOnLoad() {
   const storage = JSON.parse(localStorage.getItem("cartData")) || [];
-  const counterElement = document.querySelector('.header-counter');
+  const counterElements = document.querySelectorAll('.header-counter, .cart-counter');
 
-    if (counterElement) {
-    counterElement.textContent = storage.length.toString();
-  } else {
-    console.error("Element with class 'header-counter' not found.");
-  }
+
+  counterElements.forEach((element) => {
+    if (element) {
+      element.textContent = storage.length.toString();
+    } else {
+      console.error("Element with class 'header-counter' not found.");
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
